49e79917fc3037502ab6f82609de9fdba8ffd213
Dang Mai <contact@dangmai.net>
2021-11-28 09:31:54 -0500

    Try not expanding kubectl at shell start

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,4 @@ alias reload!='. ~/.zshrc'
 alias dc=docker-compose
 alias nr='npm run'
 alias k=kubectl
-alias kb="kubectl exec -it $(kubectl get pods | grep beets | cut -d' ' -f1 | head -n1) -- beet"
+alias kb="kubectl exec -it \$(kubectl get pods | grep beets | cut -d' ' -f1 | head -n1) -- beet"
