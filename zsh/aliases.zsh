fb47e4b9640d589163eccdbfee65b19711b16ebe
Dang Mai <contact@dangmai.net>
2022-09-16 10:59:25 -0700

    Add alias for home cluster

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias dc=docker-compose
 alias nr='npm run'
 alias k=kubectl
 alias kb="kubectl exec -it \$(kubectl get pods | grep beets | cut -d' ' -f1 | head -n1) -- beet"
+alias kh="kubectl --context=home"
