f563322dcdbf1e82dd5c27ec1b2d4306312524fe
Dang Mai <contact@dangmai.net>
2023-02-28 15:38:37 -0800

    Fix beets pod name

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias reload!='. ~/.zshrc'
 alias dc=docker-compose
 alias nr='npm run'
 alias k=kubectl
-alias beet="kubectl --context=home exec -it \$(kubectl get pods | grep beets | cut -d' ' -f1 | head -n1) -- beet"
+alias beet="kubectl --context=home exec -it \$(kubectl get pods | grep beets-home | cut -d' ' -f1 | head -n1) -- beet"
 alias kh="kubectl --context=home"
 alias ks="kubectl --context=seedbox"
 alias flexget="kubectl --context=seedbox exec -it \$(kubectl --context=seedbox get pods | grep flexget | cut -d' ' -f1 | head -n1) -- flexget"
