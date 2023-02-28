ea6dc5eb4c9be5a89c6f20fe2b31bde09dab1d9b
Dang Mai <contact@dangmai.net>
2023-02-27 17:33:46 -0800

    flexget alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias k=kubectl
 alias kb="kubectl exec -it \$(kubectl get pods | grep beets | cut -d' ' -f1 | head -n1) -- beet"
 alias kh="kubectl --context=home"
 alias ks="kubectl --context=seedbox"
+alias flexget="kubectl --context=seedbox exec -it \$(kubectl --context=seedbox get pods | grep flexget | cut -d' ' -f1 | head -n1) -- flexget"
