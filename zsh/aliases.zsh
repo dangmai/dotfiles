ef3a91a45e88530b1640b81fdc7bcd5013444417
Dang Mai <contact@dangmai.net>
2024-05-25 19:51:11 -0700

    [zsh] fix alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,4 +5,4 @@ alias k=kubectl
 alias beet="kubectl --context=home exec -it \$(kubectl get pods | grep beets-home | cut -d' ' -f1 | head -n1) -- beet"
 alias kh="kubectl --context=home"
 alias ks="kubectl --context=seedbox"
-alias flexget="kubectl --context=seedbox exec -it \$(kubectl --context=seedbox get pods | grep flexget | cut -d' ' -f1 | head -n1) -c flexget-seedbox -- flexget"
+alias flexget="kubectl --context=seedbox exec -it \$(kubectl --context=seedbox get pods | grep flexget | cut -d' ' -f1 | head -n1) -c main -- flexget"
